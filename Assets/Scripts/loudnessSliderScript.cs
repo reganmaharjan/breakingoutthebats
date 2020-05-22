bd9ef8c0a390b6e6ed00d766713a2b02be130f57
regan <regan.maharjan@olenepal.org>
2020-05-22 16:37:53 +0545

    bats

---
+++ Assets/Scripts/loudnessSliderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loudnessSliderScript : MonoBehaviour
+{
+    public Slider sliderInstance;
+    public Slider audiopitch;
+
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        audioSource.pitch = audiopitch.value;
+        Debug.Log(audioSource.pitch);
+    }
+
+    public void OnValueChanged(float value)
+    {
+        audioSource.volume = value;
+        audioSource.Play();
+    }
+
+}
---
+++ Assets/Scripts/ptichSliderScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ptichSliderScript : MonoBehaviour
+{
+
+    public Slider sliderInstance;
+    public Slider audiovolume;
+
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.volume = audiovolume.value;
+        Debug.Log(audioSource.volume);
+    }
+
+    public void OnValueChanged(float value)
+    {
+        audioSource.pitch = value;
+        audioSource.Play();
+    }
+
+}
